e3aabf62a84972152fe171fe4a5ecb55a34c2052
paolov1928 <ventura.paolo@gmail.com>
2019-01-10 11:38:26 +0000

    Done.

---
+++ src/Cell.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+export default class Cell extends Component {
+  constructor(props) {
+  super(props)
+  // ...define initial state with a key of 'color' set to the 'value' prop
+  this.state = {
+    color : this.props.value
+  }
+  }
+
+  handleClick = (e) => {
+    this.setState({
+      color: '#333'
+    })
+  }
+
+
+  render() {
+    return (
+      <div className="cell" style={{backgroundColor: `${this.state.color}`}} onClick={this.handleClick}>
+      </div>
+    )
+  }
+
+}
